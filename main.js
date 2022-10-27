332fdb3c4f07f5f0250b46b7da08c8f984454112
MatthewChildress <98126674+ArchdukeChilly@users.noreply.github.com>
2022-10-27 08:12:37 -0400

    formatted and commented code

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+// main.js true purpose is to import and call functions to keep the javascript modularized and tidy
 import { openSkills } from "./components/accordian.js";
 import { darkMode } from "./components/darkMode.js"
+
+
 
 openSkills();
 darkMode();
