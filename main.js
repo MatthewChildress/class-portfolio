e6cfb6a61589b3cdfaef6e7522175620ea0b5d0a
MatthewChildress <98126674+ArchdukeChilly@users.noreply.github.com>
2022-10-29 12:33:41 -0400

    started prototype for scroll to top. fixed nav link underline anmiation

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // main.js true purpose is to import and call functions to keep the javascript modularized and tidy
 import { openSkills } from "./components/accordian.js";
 import { darkMode } from "./components/darkMode.js"
+import { scrollToTop, showScrollButton } from './components/scrollToTop.js'
 
+const scrollBtn = document.getElementById('scrollBtn');
 
+scrollBtn.addEventListener('click', scrollToTop);
+
+window.onscroll = showScrollButton();
 
 openSkills();
 darkMode();
